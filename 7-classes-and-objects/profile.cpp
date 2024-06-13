f525a0036135d18018db427de444b0f721180a9b
TomCode <87766694+TomDevX@users.noreply.github.com>
2024-06-13 16:29:54 +0700

    Update profile.cpp
    Added profile header and custom length for its divider

--- 7-classes-and-objects/profile.cpp
+++ 7-classes-and-objects/profile.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-
+#include <vector>
 #include "profile.hpp"
 
-Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns)
+Profile::Profile(std::string new_name, int new_age,std::string new_city, std::string new_country, std::string new_pronouns) 
   : name(new_name), age(new_age), city(new_city), country(new_country), pronouns(new_pronouns) {
 
-  if (new_age >= 18) {
-    age = new_age;
-  } else {
-    age = 0;
-  }
-
 }
 
-std::string Profile::view_profile() {
-
-  std::string bio = "Name: " + name;
-  bio += "\nAge: " + std::to_string(age);
-  bio += "\nPronouns: " + pronouns;
-  std::string hobby_string = "Hobbies:\n";
-
-  for (std::string hobby : hobbies) {
-
-    hobby_string += " - " + hobby + "\n";
-
+std::string Profile::view_profile(){
+  std::vector<std::string> divide = {"========"};
+  for (int i = 0; i < name.size(); i++){
+    divide.push_back("=");
   }
 
-  return bio + "\n" + hobby_string;
+  for(std::string s : divide){
+    std::cout << s;
+  }
+  std::cout << "\n" << name << " profile\n";
+  for(std::string s2 : divide){
+    std::cout << s2;
+  }
+  std::cout << "\n";
+  
+  std::string bio = "Name: " + name + "\nAge: " + std::to_string(age) + "\nPronouns: " + pronouns + "\n";
 
+  std::string hobby_string = "Hobbies: \n";
+  for (std::string hobby : hobbies){
+    hobby_string += "- " + hobby + "\n";
+  }
+
+  return bio + hobby_string;
 }
 
-void Profile::add_hobby(std::string new_hobby) {
-
+void Profile::add_hobby(std::string new_hobby){
   hobbies.push_back(new_hobby);
-
 }
+
